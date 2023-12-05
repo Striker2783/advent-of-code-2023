339eeca168ed3cbea0a80114d8e1ab909e22f188
Striker2783 <owenxingli@gmail.com>
2023-12-05 14:52:54 -0500

    Day 5 Star 2

--- src/five.rs
+++ src/five.rs
@@ -10,6 +10,7 @@ pub fn run(file: &Path) {
     };
     let maps = Maps::parse(&contents);
     println!("Star 1: {}", maps.solve_one());
+    println!("Star 2: {}", maps.solve_two_naive());
 }
 #[derive(Default, Debug)]
 struct Maps {
@@ -18,15 +19,18 @@ struct Maps {
 }
 impl Maps {
     pub fn solve_one(&self) -> u64 {
-        let mut destinations = self.seeds.clone();
-        for v in &self.maps {
-            let mut new_destinations = vec![];
-            for dest in destinations {
-                new_destinations.push(Self::get_destination(dest, v));
-            }
-            destinations = new_destinations;
-        }
-        *destinations.iter().min().unwrap()
+        self.seeds
+            .clone()
+            .iter()
+            .map(|i| {
+                let mut i = *i;
+                for v in &self.maps {
+                    i = Self::get_destination(i, v);
+                }
+                i
+            })
+            .min()
+            .unwrap()
     }
     fn get_destination(source: u64, vec: &[(u64, u64, u64)]) -> u64 {
         for stuff in vec {
@@ -37,19 +41,51 @@ impl Maps {
         }
         source
     }
-    pub fn solve_two(&self) -> u64 {
-        let mut destinations = self.seeds.clone();
+
+    pub fn solve_two_naive(&self) -> u64 {
+        let mut destinations = vec![];
         for seed in self.seeds.chunks_exact(2) {
-            let (seed1, seed2) = (seed[0], seed[1]);
+            destinations.push((seed[0], seed[1]));
         }
-        for v in &self.maps {
-            let mut new_destinations = vec![];
-            for dest in destinations {
-                new_destinations.push(Self::get_destination(dest, v));
+        destinations
+            .iter()
+            .map(|&(low, len)| {
+                (low..(low + len))
+                    .map(|mut i| {
+                        for v in &self.maps {
+                            i = Self::get_destination(i, v);
+                        }
+                        i
+                    })
+                    .min()
+                    .unwrap()
+            })
+            .min()
+            .unwrap()
+    }
+    pub fn solve_two(&self) -> u64 {
+        let mut destinations = vec![];
+        for seed in self.seeds.chunks_exact(2) {
+            destinations.push((seed[0], seed[0] + seed[1] - 1));
+        }
+        destinations
+            .iter()
+            .map(|&r| self.recursive_two(r, 0))
+            .min()
+            .unwrap()
+    }
+    fn recursive_two(&self, range: (u64, u64), i: usize) -> u64 {
+        let mut min = u64::MAX;
+        for target in &self.maps[i] {
+            if range.1 < target.1 || range.0 >= target.1 + target.2 {
+                continue;
             }
-            destinations = new_destinations;
+            let min_r = range.0.max(target.1);
+            let max = range.1.min(target.1 + target.2 - 1);
+            let nums = (min_r - target.1 + target.0, max - target.1 + target.0);
+            min = min.min(self.recursive_two(nums, i + 1));
         }
-        *destinations.iter().min().unwrap()
+        min
     }
     fn in_range(range: (u64, u64), num: u64) -> bool {
         range.0 <= num && num < range.0 + range.1
@@ -98,5 +134,12 @@ mod tests {
             "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4",
         );
         assert_eq!(map.solve_one(), 35);
+    }
+    #[test]
+    fn test_solve_two() {
+        let map = Maps::parse(
+            "seeds: 79 14 55 13\n\nseed-to-soil map:\n50 98 2\n52 50 48\n\nsoil-to-fertilizer map:\n0 15 37\n37 52 2\n39 0 15\n\nfertilizer-to-water map:\n49 53 8\n0 11 42\n42 0 7\n57 7 4\n\nwater-to-light map:\n88 18 7\n18 25 70\n\nlight-to-temperature map:\n45 77 23\n81 45 19\n68 64 13\n\ntemperature-to-humidity map:\n0 69 1\n1 0 69\n\nhumidity-to-location map:\n60 56 37\n56 93 4",
+        );
+        assert_eq!(map.solve_two(), 46);
     }
 }
